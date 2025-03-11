5363911df78debd82a0c5efa49ddf11543cc83ec
Patrick Shim <patrick.shim91@yahoo.com>
2025-03-10 23:55:32 -0400

    Updated serializers

--- app/controllers/Api/V1/subscriptions_controller.rb
+++ app/controllers/Api/V1/subscriptions_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
     subscriptions = Subscription.all.order(:id)
-    render json: subscriptions
+    render json: SubscriptionSerializer.new(subscriptions).serializable_hash
   end
 
   def show
     subscription = Subscription.find_by(id: params[:id])
 
     if subscription
-      render json: subscription, status: :ok
+      render json: SubscriptionSerializer.new(subscription).serializable_hash
     else
       render json: { error: "Subscription not found." }, status: :not_found
     end
@@ -21,7 +21,11 @@ class Api::V1::SubscriptionsController < ApplicationController
       subscription.update(subscription_params)
 
       if subscription.save
-        render json: subscription, status: :ok
+        render json: {
+          subscription: SubscriptionSerializer.new(subscription).serializable_hash
+        }, status: :ok
+      else
+        render json: { error: "Subscription failed to cancel." }, status: :unprocessable_entity
       end
     else
       render json: { error: "Subscription not found." }, status: :not_found
--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -2,6 +2,28 @@ class SubscriptionSerializer
   include JSONAPI::Serializer
   attributes :id, :title, :price, :status, :frequency
 
-  has_many :teas
-  has_many :customers
+  attribute :teas do |subscription|
+    subscription.teas.map do |tea|
+      {
+        id: tea.id,
+        title: tea.title,
+        description: tea.description,
+        temperature: tea.temperature,
+        brew_time: tea.brew_time,
+        price: tea.price
+      }
+    end
+  end
+
+  attribute :customers do |subscription|
+    subscription.customers.map do |customer|
+      {
+        id: customer.id,
+        first_name: customer.first_name,
+        last_name: customer.last_name,
+        email: customer.email,
+        address: customer.address
+      }
+    end
+  end
 end
